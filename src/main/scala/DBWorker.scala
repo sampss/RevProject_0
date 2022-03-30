25d7f7eeee53fc75df1c2bbe7ed641a7f0db9991
sampss <sampsons79@gmail.com>
2022-03-30 14:02:16 -0400

    just updating and testing

--- src/main/scala/DBWorker.scala
+++ src/main/scala/DBWorker.scala
@@ -8,7 +8,7 @@ object DBWorker extends UserSettings{
 
   def main(args: Array[String]) {
 
-    // test again
+    // test again 3
     try {
       // make the connection
       Class.forName(driver)
